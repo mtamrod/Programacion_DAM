6de8cfc58a5da3868832f86205b1e1e253b176c7
mtamrod534 <trmario@iesrafaelalberti.es>
2024-09-30 19:14:29 +0200

    Actualización 1.2.9.py

--- ejercicios-u1-mtamord/P1.2 - Ejercicios básicos/Ejercicio 1.2.9.py
+++ ejercicios-u1-mtamord/P1.2 - Ejercicios básicos/Ejercicio 1.2.9.py
@@ -1,6 +1,6 @@
 print("La suma de los tres números es:", 
-      input("Introduce el primer número: ") + 
-      input("Introduce el segundo número: ") + 
-      input("Introduce el tercer número: "))
+      int(input("Introduce el primer número: ")) + 
+      int(input("Introduce el segundo número: ")) + 
+      int(input("Introduce el tercer número: ")))
 
 #No ha sido posible
